f16d6948b16d57445fb7fda5c55bc823c216ea87
xuluheng <xuluheng634@163.com>
2019-04-24 01:55:28 +0000

    基础页面结构

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE-edge">
+  <meta name="viewport" content="width-device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>@yield('title', 'laraBBS') - Laravel 进阶教程</title>
+
+  <!-- Styles -->
+  <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+
+</head>
+
+<body>
+  <div id="app" class="{{ route_class() }}-page">
+
+    @include('layouts._header')
+
+    <div class="container">
+
+      @include('shared._messages')
+
+      @yield('content')
+
+    </div>
+
+    @include('layouts._footer')
+  </div>
+
+  <!-- Scripts -->
+  <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+</body>
+
+</html>
